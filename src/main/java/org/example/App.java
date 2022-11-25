34e2b431d1e5f550b50da5bfbbb7eec172ddc913
bg.kz <zhou1770763@163.com>
2022-11-25 16:56:04 +0800

    master3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,5 +10,6 @@ public class App {
         System.out.println( "Hello World!" );
         System.out.println("hello 111");
         System.out.println("加油！！");
+        System.out.println("master");
     }
 }
